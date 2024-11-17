957e46984f918223aee116bdaf567f673bcb20d0
adnan-mohammed7 <mohammedadnanaz7@gmail.com>
2024-11-16 20:19:52 -0500

    Login Page UI design completed and added libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,9 @@ android {
 }
 
 dependencies {
-
+    implementation("androidx.room:room-runtime:2.6.0")
+    annotationProcessor("androidx.room:room-compiler:2.6.0")
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
--- app/src/main/java/com/example/taxfilemanagementapp/MainActivity.java
+++ app/src/main/java/com/example/taxfilemanagementapp/MainActivity.java
@@ -1,24 +1,32 @@
 package com.example.taxfilemanagementapp;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        ImageView imageView = findViewById(R.id.loginImageViewer);
+        Button signupBtn = findViewById(R.id.signupBtn);
+
+        Glide.with(this).load("https://www.gtawealth.ca/sites/default/files/styles/large/public/Tax-Planning_Tax-Returns_GTA-Wealth_9.jpg?itok=L1r5jPfE").into(imageView);
+
+    }
+
+    public void handleSignup(View view) {
+        System.out.println("Signup");
+    }
+
+    public void handleLogin(View view) {
+        System.out.println("Login");
     }
 }
