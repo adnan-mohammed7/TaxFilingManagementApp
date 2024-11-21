c63e2af4782bc3c48d6995e3afd050d6832c1a7f
adnan-mohammed7 <mohammedadnanaz7@gmail.com>
2024-11-20 19:13:14 -0500

    Google Maps added to customer details screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,6 +35,8 @@ dependencies {
     implementation("androidx.room:room-runtime:2.6.0")
     annotationProcessor("androidx.room:room-compiler:2.6.0")
     implementation ("com.github.bumptech.glide:glide:4.16.0")
+    implementation("com.google.android.gms:play-services-maps:18.2.0")
+    implementation("com.google.android.gms:play-services-location:21.0.1")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
--- app/src/main/java/com/example/taxfilemanagementapp/CustomerDetailActivity.java
+++ app/src/main/java/com/example/taxfilemanagementapp/CustomerDetailActivity.java
@@ -1,18 +1,35 @@
 package com.example.taxfilemanagementapp;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.LatLng;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class CustomerDetailActivity extends AppCompatActivity {
+public class CustomerDetailActivity extends AppCompatActivity implements OnMapReadyCallback{
     TextView nameField;
     TextView userNameField;
     TextView phoneField;
@@ -28,10 +45,26 @@ public class CustomerDetailActivity extends AppCompatActivity {
     Toolbar toolbar;
 
     UserServices userServices;
-    Customer loggedInCustomer;
+    Customer selectedCustomer;
     String status;
     String[] statusOptions = {"AWAITED", "FAILEDTOREACH", "ONBOARDED", "INPROCESS", "COMPLETED","DENIED"};
     int position = -1;
+
+    private GoogleMap mMap;
+    private FusedLocationProviderClient mLocationClient;
+    private LatLng currentLocation;
+    boolean markerAdded = false;
+
+    private final ActivityResultLauncher<String[]> requestPermissionLauncher =
+            registerForActivityResult(new ActivityResultContracts.RequestMultiplePermissions(), result -> {
+                Boolean fineLocationGranted = result.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false);
+                Boolean coarseLocationGranted = result.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false);
+                if (fineLocationGranted != null && fineLocationGranted || coarseLocationGranted != null && coarseLocationGranted) {
+                    fetchLastLocation();
+                }else{
+                    Toast.makeText(this, "Location Access Denied", Toast.LENGTH_SHORT).show();
+                }
+            });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +76,15 @@ public class CustomerDetailActivity extends AppCompatActivity {
 
         if(getSupportActionBar() != null){
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+
+        mLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        requestPermissionLauncher.launch(new String[] {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION});
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(this);
         }
 
         nameField = findViewById(R.id.customerDetailName);
@@ -68,21 +110,22 @@ public class CustomerDetailActivity extends AppCompatActivity {
         position = intent.getIntExtra("position", -1);
 
         userServices.getCustomerByUsername(intent.getStringExtra("username"), customer -> runOnUiThread(()->{
-            loggedInCustomer = customer;
+            selectedCustomer = customer;
             preLoadField();
+            setCustomerLocation();
         }));
     }
 
     public void updateCustomer(View view) {
         setValue();
 
-        loggedInCustomer.setStatus(status);
-        userServices.updateCustomer(loggedInCustomer, new UserServices.OperationCallback() {
+        selectedCustomer.setStatus(status);
+        userServices.updateCustomer(selectedCustomer, new UserServices.OperationCallback() {
             @Override
             public void onOperationCompleted() {
                 runOnUiThread(()-> Toast.makeText(CustomerDetailActivity.this, "Status Updated!", Toast.LENGTH_SHORT).show());
                 Intent intent = new Intent();
-                intent.putExtra("id", loggedInCustomer.uid);
+                intent.putExtra("id", selectedCustomer.uid);
                 intent.putExtra("position", position);
                 setResult(RESULT_OK, intent);
                 finish();
@@ -100,18 +143,18 @@ public class CustomerDetailActivity extends AppCompatActivity {
     }
 
     void preLoadField(){
-        nameField.setText(String.format("Name: %s", loggedInCustomer.name));
-        userNameField.setText(String.format("Username: %s", loggedInCustomer.userName));
-        emailField.setText(String.format("Email: %s", loggedInCustomer.email));
-        phoneField.setText(String.format("Phone: %s", loggedInCustomer.phone));
-        companyField.setText(String.format("Company: %s", loggedInCustomer.companyTitle));
-        websiteField.setText(String.format("Website: %s", loggedInCustomer.website));
-        suiteField.setText(String.format("Suite: %s", loggedInCustomer.address.suite));
-        streetField.setText(String.format("Street: %s", loggedInCustomer.address.street));
-        cityField.setText(String.format("City: %s", loggedInCustomer.address.city));
-        provinceField.setText(String.format("Province: %s", loggedInCustomer.address.province));
-        postalField.setText(String.format("Postal Code: %s", loggedInCustomer.address.zipcode));
-        int pos = new ArrayList<>(Arrays.asList(statusOptions)).indexOf(loggedInCustomer.status);
+        nameField.setText(String.format("Name: %s", selectedCustomer.name));
+        userNameField.setText(String.format("Username: %s", selectedCustomer.userName));
+        emailField.setText(String.format("Email: %s", selectedCustomer.email));
+        phoneField.setText(String.format("Phone: %s", selectedCustomer.phone));
+        companyField.setText(String.format("Company: %s", selectedCustomer.companyTitle));
+        websiteField.setText(String.format("Website: %s", selectedCustomer.website));
+        suiteField.setText(String.format("Suite: %s", selectedCustomer.address.suite));
+        streetField.setText(String.format("Street: %s", selectedCustomer.address.street));
+        cityField.setText(String.format("City: %s", selectedCustomer.address.city));
+        provinceField.setText(String.format("Province: %s", selectedCustomer.address.province));
+        postalField.setText(String.format("Postal Code: %s", selectedCustomer.address.zipcode));
+        int pos = new ArrayList<>(Arrays.asList(statusOptions)).indexOf(selectedCustomer.status);
         statusView.setSelection(pos);
     }
 
@@ -123,5 +166,40 @@ public class CustomerDetailActivity extends AppCompatActivity {
     public boolean onSupportNavigateUp(){
         finish();
         return true;
+    }
+
+    private void fetchLastLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                || ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+
+            mLocationClient.getLastLocation().addOnSuccessListener(this, location -> {
+                if (location != null) {
+                    currentLocation = new LatLng(location.getLatitude(), location.getLongitude());
+
+                    // Move the camera to the current location
+                    if (mMap != null) {
+                        mMap.setMyLocationEnabled(true);
+                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 15));
+                    }
+                }
+            });
+        }
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        setCustomerLocation();
+    }
+
+    private void setCustomerLocation(){
+        if(mMap != null && selectedCustomer != null){
+            if(!markerAdded){
+                LatLng customerLocation = new LatLng(selectedCustomer.address.geo.lat, selectedCustomer.address.geo.lon);
+                mMap.addMarker(new MarkerOptions().position(customerLocation).title(selectedCustomer.name));
+            }
+            markerAdded = true;
+        }
     }
 }
